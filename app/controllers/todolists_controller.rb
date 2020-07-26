11e4593349476d1183503927397523bc5e8858f3
taiki-shi <love.hurricane.exist@gmail.com>
2020-07-26 17:11:56 +0900

    update for chapter9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -3,7 +3,6 @@ class TodolistsController < ApplicationController
   	@list = List.new
   end
 
-
   def create
   	list = List.new(list_params)
   	list.save
@@ -26,6 +25,11 @@ class TodolistsController < ApplicationController
   	list = List.find(params[:id])
   	list.update(list_params)
   	redirect_to todolist_path(list.id)
+  end
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to todolists_path
   end
 
   private
