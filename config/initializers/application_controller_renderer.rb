42493bfe3a5b22fb7572884e4e97c2817a70eeea
taiki-shi <love.hurricane.exist@gmail.com>
2020-07-26 16:47:55 +0900

    update for chapter8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '47c9d0341cf0d5828bf36df04e9c9fd8b33439ed49bef19515bf7f5ca5d4a8ed5eec11fb4adcfaba35ac1fef898b9d83fa687952573a6011f8da9441871c04d0'
